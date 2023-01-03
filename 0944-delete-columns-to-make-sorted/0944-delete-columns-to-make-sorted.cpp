c481c9fd92a3a733f8fdb3adcdffcac45178ed40
Rahul Kumar <75949459+rahulrj1@users.noreply.github.com>
2023-01-03 13:14:44 +0530

    Time: 1534 ms (5.18%), Space: 494.9 MB (5.80%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -3,11 +3,14 @@ public:
     int minDeletionSize(vector<string>& strs) {
         int ans = 0;
         for(int i=0; i<strs[0].length(); i++) {
-            string word = "";
-            for(string s: strs) word += s[i];
-            string sortedWord = word;
-            sort(sortedWord.begin(), sortedWord.end());
-            if(word != sortedWord) ans++;
+            char c = 'a' - 1; 
+            for(string s: strs) {
+                if(s[i] < c) {
+                    ans++; 
+                    break;
+                }
+                c = s[i];
+            }
         }
         return ans;
     }
