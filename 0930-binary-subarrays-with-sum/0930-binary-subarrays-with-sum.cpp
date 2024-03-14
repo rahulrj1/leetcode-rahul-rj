e1ea56f6c5d6290ffd402797989183c186b1b3ee
Rahul Kumar <75949459+rahulrj1@users.noreply.github.com>
2024-03-14 20:53:54 +0530

    Time: 16 ms (94.16%), Space: 32.3 MB (49.59%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,51 +1,12 @@
 class Solution {
 public:
-    int findNext(vector<int> &nums, int idx) {
-        for(int i=idx; i<nums.size(); i++) {
-            if(nums[i] == 1) return i;
-        }
-        return nums.size();
-    }
-    
-    int findAnsWithGoalZero(vector<int> &nums) {
-        int res = 0, cnt = 0;
-        for(int i=0; i<nums.size(); i++) {
-            if(nums[i] == 0) cnt++;
-            else {
-                res += (cnt+1)*(cnt)/2;
-                cnt = 0;
-            }
-        }
-        res += (cnt+1)*(cnt)/2;
-        return res;
-    }
-    
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int n = nums.size(); if(goal == 0) return findAnsWithGoalZero(nums);
-        
-        int prevZeros = 0, nextZeros = 0, i = 0, j = 0, sum = goal;
-        while(i<n && nums[i] == 0) i++, prevZeros++;
-        j = i;
-        while(sum > 0 && j < n) {
-            if(nums[j] == 1) sum--;
-            j++;
-        } 
-        
-        if(sum > 0) return 0;
-        int z = findNext(nums, j), res = 0;
-        while(1) {
-            // (z - j) zeroes
-            nextZeros = (z-j);
-            res += (prevZeros + 1)*(nextZeros + 1);
-            cout<< (prevZeros + 1)*(nextZeros + 1) << " ";
-            
-            
-            if(j >= n) break; 
-            j = z+1;
-            int nextI = findNext(nums, i+1);
-            prevZeros = (nextI - i - 1);
-            i = nextI;
-            z = findNext(nums, j);
+        int n = nums.size(), sum = 0, res = 0;
+        vector<int> cntSum(n+1, 0); cntSum[0]++;
+        for(int i=0; i<n; i++) {
+            if(nums[i] == 1) sum++;
+            if(sum - goal >= 0) res += cntSum[sum - goal];
+            cntSum[sum]++;
         }
         return res;
     }
