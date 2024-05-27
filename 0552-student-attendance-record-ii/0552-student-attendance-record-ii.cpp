d827cac76c5420156d3c1b143e3cf81c41a8dec7
Rahul Kumar <75949459+rahulrj1@users.noreply.github.com>
2024-05-28 01:24:36 +0530

    Time: 1022 ms (35.64%), Space: 386.7 MB (38.88%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -1,28 +1,26 @@
 class Solution {
     int mod = 1e9 + 7;
-    vector<vector<vector<int>>> memo;
 public:
-    int helper(int n, int absentDays, int lastConsLate) {
-        // base case
-        if(absentDays >= 2 || lastConsLate >= 3) return 0;
-        if(n == 0) return 1;
-        
-        if(memo[n][absentDays][lastConsLate] != -1) return memo[n][absentDays][lastConsLate];
-        
-        // P
-        int cnt = helper(n-1, absentDays, 0);
-        
-        // A
-        cnt = (cnt + helper(n-1, absentDays+1, 0)) % mod;
-        
-        // L
-        cnt = (cnt + helper(n-1, absentDays, lastConsLate+1)) % mod;
-        
-        return memo[n][absentDays][lastConsLate] = cnt;
-    }
-    
     int checkRecord(int n) {
-        memo = vector<vector<vector<int>>> (n+1, vector<vector<int>> (2, vector<int> (3, -1)));
-        return helper(n, 0, 0);
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (2, vector<int> (3, 0)));
+        dp[0][0][0] = 1; // base case
+        
+        for(int len=0; len<n; len++) {
+            for(int a=0; a<2; a++) {
+                for(int l=0; l<3; l++) {
+                    dp[len+1][a][0] = (dp[len+1][a][0] + dp[len][a][l]) % mod;
+                    if(a == 0) dp[len+1][a+1][0] = (dp[len+1][a+1][0] + dp[len][a][l]) % mod;
+                    if(l < 2) dp[len+1][a][l+1] = (dp[len+1][a][l+1] + dp[len][a][l]) % mod;
+                }
+            }
+        }
+        
+        int res = 0;
+        for(int a=0; a<2; a++) {
+            for(int l=0; l<3; l++) {
+                res = (res + dp[n][a][l]) % mod;
+            }
+        }
+        return res;
     }
 };
