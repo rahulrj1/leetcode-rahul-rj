c63c88f62609ab267b363988a540dbb4ceac0f1d
Rahul Kumar <75949459+rahulrj1@users.noreply.github.com>
2024-06-11 01:25:37 +0530

    Time: 1250 ms (59.26%), Space: 7.9 MB (62.91%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -10,7 +10,7 @@ public:
         if(idx == s.length() ) return 0;
         if(dp[idx] != -1) return dp[idx];
         int res = s.length() - idx - 1;
-        for(int i=idx; i<s.length(); i++) {
+        for(int i=idx; i<s.length() - 1; i++) {
             if(isPal(s, idx, i)) {
                 res = min(res, 1 + helper(s, i+1));
             }
