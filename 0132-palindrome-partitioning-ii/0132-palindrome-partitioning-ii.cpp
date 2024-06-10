00a7a3c5899dfd76ed11dcbe30cd299e7e5436e6
Rahul Kumar <75949459+rahulrj1@users.noreply.github.com>
2024-06-11 01:24:00 +0530

    Time: 1287 ms (51.55%), Space: 7.9 MB (53.44%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> dp;
+    bool isPal(string &s, int l, int r) {
+        while(l<r) {if(s[l] != s[r]) return false; else l++, r--;}
+        return true;
+    }
+    
+    int helper(string &s, int idx) {
+        if(idx == s.length() ) return 0;
+        if(dp[idx] != -1) return dp[idx];
+        int res = s.length() - idx - 1;
+        for(int i=idx; i<s.length(); i++) {
+            if(isPal(s, idx, i)) {
+                res = min(res, 1 + helper(s, i+1));
+            }
+        }
+        if(isPal(s, idx, s.length()-1)) res = 0;
+        return dp[idx] = res;
+    }
+    
+    
+    int minCut(string s) {
+        vector<int> v(s.length() + 1, -1);
+        dp = v;
+        return helper(s, 0);
+    }
+};
