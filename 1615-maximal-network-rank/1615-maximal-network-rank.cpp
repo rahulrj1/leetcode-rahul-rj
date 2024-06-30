241d98427faecb2b47cd098882da395992d6110d
Rahul Kumar <75949459+rahulrj1@users.noreply.github.com>
2024-06-30 11:40:51 +0530

    Time: 51 ms (82.67%), Space: 34.8 MB (69.64%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        unordered_map<int, vector<int>> adjlist;
-        for(vector<int> &road: roads) adjlist[road[0]].push_back(road[1]), adjlist[road[1]].push_back(road[0]);
+        vector<int> rank(n, 0);
+        vector<vector<bool>> adjmat(n, vector<bool> (n, false));
+        for(vector<int> road: roads) rank[road[0]]++, rank[road[1]]++, adjmat[road[0]][road[1]] = true, adjmat[road[1]][road[0]] = true;
+        
         
         int maxRank = 0;
         for(int i=0; i<n; i++) {
             for(int j=i+1; j<n; j++) {
-                int rank_i = adjlist[i].size(), rank_j = adjlist[j].size();
-                int isRoad = 0;
-                for(int nbr: adjlist[i]) if(nbr == j) {isRoad = 1; break;}
-                maxRank = max(maxRank, rank_i + rank_j - isRoad);
+                maxRank = max(maxRank, rank[i] + rank[j] - adjmat[i][j]);
             }
         }
         return maxRank;
