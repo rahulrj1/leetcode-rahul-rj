f19c732e33f8f14b75446aa2db08bf9980ccac94
Rahul Kumar <75949459+rahulrj1@users.noreply.github.com>
2024-09-12 11:23:23 +0530

    Time: 93 ms (70.26%), Space: 72.5 MB (75.82%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
-        int res = 1, left = 0;
-        deque<int> incq; incq.push_back(0);
-        deque<int> decq; decq.push_back(0);
+        int l=0, r=0, res=1;
+        deque<int> inc; inc.push_back(0);
+        deque<int> dec; dec.push_back(0);
+        
         for(int i=1; i<nums.size(); i++) {
-            while(!incq.empty() && nums[incq.back()] > nums[i]) incq.pop_back();
-            while(!decq.empty() && nums[decq.back()] < nums[i]) decq.pop_back();
             
-            incq.push_back(i);
-            decq.push_back(i);
+            // dec queue
+            while(!dec.empty() && nums[dec.back()] <= nums[i]) dec.pop_back();
+            dec.push_back(i);
             
-            int maxEle = nums[decq.front()];
-            int minEle = nums[incq.front()];
+            // inc queue
+            while(!inc.empty() && nums[inc.back()] >= nums[i]) inc.pop_back();
+            inc.push_back(i);
             
-            while(maxEle - minEle > limit) {
-                if(incq.front() > decq.front()) left = decq.front() + 1, decq.pop_front();
-                else left = incq.front() + 1, incq.pop_front();
+            r = i;
+            while(true) {
+                int Max = nums[dec.front()];
+                int min = nums[inc.front()];
                 
-                maxEle = nums[decq.front()], minEle = nums[incq.front()];
+                if(Max-min <= limit) {res = max(res, r - l + 1); break;}
+                if(inc.front() == l) inc.pop_front();
+                if(dec.front() == l) dec.pop_front();
+                l++;
             }
-            res = max(res, i + 1 - left);
+            
         }
         return res;
     }
